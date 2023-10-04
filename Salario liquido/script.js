1e9324d89dbf387570a6aba7cf4a345baf93f40e
Arthur <74980776+arth-inacio@users.noreply.github.com>
2023-10-04 00:06:48 -0300

    feat: Adicionando calculo da recisão

--- Salario liquido/script.js
+++ Salario liquido/script.js
@@ -63,16 +63,23 @@ function ferias() {
 
 function recisao() {
     let salariob = document.getElementById('salariobruto')
+    let mesesTrabalhados = document.getElementById('mesesTrabalhados')
     let res = document.getElementById('res')
 
     if (salariob.value == 0) {
         res.innerHTML = `Valor Inválido!`
         window.alert('[Erro] Faltam dados!')
     }else {
+        // Considerando que em um mes de 30 dias trabalhamos apenas 21 dias
         let sb = Number(salariob.value)
-        let recis 
+        let meses_trab = Number(mesesTrabalhados.value)
+        let salario_dia = sb/30
+        let saldo_salario = salario_dia*21
+        let decimo_terceiro = (sb/12)*meses_trab
+        let ferias_prop = ((sb * meses_trab)/12) + 1/3
+        let recis = saldo_salario + decimo_terceiro + ferias_prop
     
-        res.innerHTML += `<strong>Salário Bruto</strong>: ${sb.toFixed(2)}<br><strong>Ferias</strong>: ${recis.toFixed(2)}`
+        res.innerHTML += `<strong>Salário Bruto</strong>: ${sb.toFixed(2)}<br><strong>Recisão</strong>: ${recis.toFixed(2)}`
     }
     res.style.textAlign = 'center'
 }
